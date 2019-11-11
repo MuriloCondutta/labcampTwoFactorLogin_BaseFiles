cd550975e3f8b8ff9c93e840a9b8a67665ce2eaa
Murilo Condutta <m.condutta@replybrasil.onmicrosoft.com>
2019-11-11 15:57:17 -0300

    Arquivos Base LabCamp v1.0

---
+++ js/FaceAPI.js
@@ -0,0 +1,40 @@
+// Chamadas Face API
+
+var CHAVE_ACESSO = "";
+var URL_BASE = "";
+var GRUPO = "";
+
+// Treinar grupo de pessoas
+function treinarGrupo() {
+
+    
+
+}
+
+// Cadastrar face
+function cadastrarFace(imagemFace, personId) {
+
+    
+
+}
+
+// Identificar pessoa
+function identificarFace(faceId, usuario) {
+
+    
+
+}
+
+// Login
+function detectarEIdentificar(imagemFace, usuario, face) {
+
+
+    
+}
+
+// Cadastro
+function criarPessoaECadastrarFace(imagemFace, usuario) {
+
+    
+
+}
